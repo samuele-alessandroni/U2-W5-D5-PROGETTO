5248c816dba937754a6d974122ae0b7cf5f6616c
samuele-alessandroni <156697340+samuele-alessandroni@users.noreply.github.com>
2024-02-17 16:28:25 +0100

    m animation add

--- base_di_partenza/index.js
+++ base_di_partenza/index.js
@@ -13,6 +13,36 @@ if (window.scrollY >= 300) {
 })
 
 
+const svg = document.getElementsByTagName("svg")[0]
+
+
+let arrM = []
+for (let i = 3; i < 1640; i+=3) {
+    const g = document.getElementsByTagName("g")[i]
+   
+        arrM.push(g)
+        
+    
+}
+
+
+const mAnimation = () =>{
+    let firstRandNumb = 0
+    let secondRandNumb = 0
+
+    firstRandNumb = Math.floor(Math.random() * 546 + 1)
+    secondRandNumb = Math.floor(Math.random() * 546 + 1)
+    const op1 = arrM[firstRandNumb]
+    const op0 = arrM[secondRandNumb]
+
+    op1.style.opacity = "1"
+    op0.style.opacity = "0"
+}
+
+
+setInterval(mAnimation, 100)
+
+
 
 
 
